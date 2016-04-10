5b92f599a18de25a1dadbe2e25e28c73eca23475
Marek Milkovič <milkovic.marek@gmail.com>
2016-04-10 16:57:15 +0200

    Added support for key-value analysis displaying.

--- web/tcp-stats.js
+++ web/tcp-stats.js
@@ -1,3 +1,9 @@
+function initDataDisplay() {
+	if (typeof(tcpstats_data) !== 'undefined') {
+		$("#no-analysis-found").remove();
+	}
+}
+
 function initMenuActions() {
 	$("#menu-analyses").click(function() {
 		$("#menu-analyses").addClass("active");
@@ -14,7 +20,53 @@ function initMenuActions() {
 	});
 }
 
-function initAnalysis() {
+function initAbout() {
+	$("#about-container").hide();
+}
+
+function displayAnalysesData() {
+	if (typeof(tcpstats_data) === "undefined") {
+		return;
+	}
+
+	$.each(tcpstats_data["analyses"], function(index, value) {
+		displayAnalysisData(value, tcpstats_data.analyses_data[value.id]);
+	});
+}
+
+function displayAnalysisData(metadata, data) {
+	var analysis = $("<div></div>").addClass("analysis");
+	var analysisHeader = $("<div></div>").addClass("analysis-header").appendTo(analysis);
+	var analysisBody = $("<div></div>").addClass("analysis-body").appendTo(analysis);
+
+	analysisHeader.text(metadata.name);
+
+	var headerArrow = $("<span></span>").addClass("glyphicon").addClass("glyphicon-menu-up")
+		.addClass("pull-right").addClass("analysis-header-arrow");
+	headerArrow.appendTo(analysisHeader);
+
+	switch (metadata.dataType) {
+		case "key_value":
+			createKeyValueData(data).appendTo(analysisBody);
+			break;
+	}
+
+	analysis.appendTo("#analysis-container");
+}
+
+function createKeyValueData(data) {
+	var table = $("<table></table>").addClass("table").addClass("table-striped").addClass("key-value-table");
+	for (i = 0; i < data.length; i++) {
+		var row = $("<tr></tr>");
+		$("<td></td>").text(data[i].key).appendTo(row);
+		$("<td></td>").text(data[i].value).appendTo(row);
+		row.appendTo(table);
+	}
+
+	return table;
+}
+
+function setupAnalysisHandlers() {
 	$(".analysis").each(function() {
 		var body = $(this).children(".analysis-body");
 		var arrow = $(this).find(".analysis-header-arrow");
@@ -36,12 +88,12 @@ function initAnalysis() {
 	});
 }
 
-function initAbout() {
-	$("#about-container").hide();
-}
-
 $(function() {
+	initDataDisplay();
 	initMenuActions();
-	initAnalysis();
 	initAbout();
+
+	displayAnalysesData();
+
+	setupAnalysisHandlers();
 });
